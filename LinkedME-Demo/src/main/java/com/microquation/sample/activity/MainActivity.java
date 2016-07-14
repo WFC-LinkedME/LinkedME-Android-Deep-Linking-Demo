7346db5f74957f38dbeebe60e80da277b976f4e4
lipeng <lipeng@linkedme.cc>
2016-07-14 12:51:11 +0800

    Code optimization

--- LinkedME-Demo/src/main/java/com/microquation/sample/activity/MainActivity.java
+++ LinkedME-Demo/src/main/java/com/microquation/sample/activity/MainActivity.java
@@ -24,6 +24,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageButton;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 
@@ -48,6 +49,7 @@ import java.util.HashMap;
 public class MainActivity extends AppCompatActivity {
 
     private LinkedME linkedME;
+
     /**
      * 应用方
      */
@@ -64,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
      * 简介
      */
     private AppCompatImageButton id_intro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,25 +86,25 @@ public class MainActivity extends AppCompatActivity {
         id_apps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, AppsActivity.class));
+                openActivity(getString(R.string.str_apps_name), getString(R.string.str_h5_apps), getString(R.string.str_share_content_apps), getString(R.string.str_path_apps), ShareActivity.class);
             }
         });
         id_features.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, FeaturesActivity.class));
+                openActivity(getString(R.string.str_features_name), getString(R.string.str_h5_features), getString(R.string.str_share_content_features), getString(R.string.str_path_features), ShareActivity.class);
             }
         });
         id_demo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, DemoActivity.class));
+                openActivity(null, null, null, null, DemoActivity.class);
             }
         });
         id_intro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, SummaryActivity.class));
+                openActivity(getString(R.string.str_intro_name), getString(R.string.str_h5_intro), getString(R.string.str_share_content_intro), getString(R.string.str_path_intro), ShareActivity.class);
             }
         });
     }
@@ -110,11 +113,14 @@ public class MainActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
 
-        //初始化LinkedME实例
-
-        linkedME = LinkedME.getInstance(this);
-        //初始化Session，获取Intent内容及跳转参数
-        linkedME.initSession(simpleInitListener, this.getIntent().getData(), this);
+        try {
+            //初始化LinkedME实例
+            linkedME = LinkedME.getInstance();
+            //初始化Session，获取Intent内容及跳转参数
+            linkedME.initSession(simpleInitListener, this.getIntent().getData(), this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -126,39 +132,52 @@ public class MainActivity extends AppCompatActivity {
     LMSimpleInitListener simpleInitListener = new LMSimpleInitListener() {
         @Override
         public void onSimpleInitFinished(LMUniversalObject lmUniversalObject, LinkProperties linkProperties, LMError error) {
-            if (error != null) {
-                Log.i("LinkedME-Demo", "LinkedME初始化失败. " + error.getMessage());
-            } else {
+            try {
+                if (error != null) {
+                    Log.i("LinkedME-Demo", "LinkedME初始化失败. " + error.getMessage());
+                } else {
 
-                //LinkedME SDK初始化成功，获取跳转参数，具体跳转参数在LinkProperties中，和创建深度链接时设置的参数相同；
-                Log.i("LinkedME-Demo", "LinkedME初始化完成");
+                    //LinkedME SDK初始化成功，获取跳转参数，具体跳转参数在LinkProperties中，和创建深度链接时设置的参数相同；
+                    Log.i("LinkedME-Demo", "LinkedME初始化完成");
 
-                if (linkProperties != null) {
-                    Log.i("LinkedME-Demo", "Channel " + linkProperties.getChannel());
-                    Log.i("LinkedME-Demo", "control params " + linkProperties.getControlParams());
+                    if (linkProperties != null) {
+                        Log.i("LinkedME-Demo", "Channel " + linkProperties.getChannel());
+                        Log.i("LinkedME-Demo", "control params " + linkProperties.getControlParams());
 
-                    //获取自定义参数封装成的hashmap对象
-                    HashMap<String, String> hashMap = linkProperties.getControlParams();
-                    //获取传入的参数
-                    String view = hashMap.get("View");
-                    if (view != null) {
-                        if (view.equals("Partner")) {
-                            startActivity(new Intent(MainActivity.this, AppsActivity.class));
-                        } else if (view.equals("Feature")) {
-                            startActivity(new Intent(MainActivity.this, FeaturesActivity.class));
-                        } else if (view.equals("Demo")) {
-                            startActivity(new Intent(MainActivity.this, DemoActivity.class));
-                        } else if (view.equals("Summary")) {
-                            startActivity(new Intent(MainActivity.this, SummaryActivity.class));
+                        //获取自定义参数封装成的hashmap对象
+                        HashMap<String, String> hashMap = linkProperties.getControlParams();
+
+                        //获取传入的参数
+                        String view = hashMap.get("View");
+                        String title = "";
+                        String shareContent = "";
+                        String url_path = "";
+                        if (view != null) {
+                            if (view.equals(getString(R.string.str_h5_apps))) {
+                                title = getString(R.string.str_apps_name);
+                                shareContent = getString(R.string.str_share_content_apps);
+                                url_path = getString(R.string.str_path_apps);
+                            } else if (view.equals(getString(R.string.str_h5_features))) {
+                                title = getString(R.string.str_features_name);
+                                shareContent = getString(R.string.str_share_content_features);
+                                url_path = getString(R.string.str_path_features);
+                            } else if (view.equals(getString(R.string.str_h5_intro))) {
+                                title = getString(R.string.str_intro_name);
+                                shareContent = getString(R.string.str_share_content_intro);
+                                url_path = getString(R.string.str_path_intro);
+                            }
+                            openActivity(title, view, shareContent, url_path, ShareActivity.class);
                         }
                     }
-                }
 
-                if (lmUniversalObject != null) {
-                    Log.i("LinkedME-Demo", "title " + lmUniversalObject.getTitle());
-                    Log.i("LinkedME-Demo", "control " + linkProperties.getControlParams());
-                    Log.i("ContentMetaData", "metadata " + lmUniversalObject.getMetadata());
+                    if (lmUniversalObject != null) {
+                        Log.i("LinkedME-Demo", "title " + lmUniversalObject.getTitle());
+                        Log.i("LinkedME-Demo", "control " + linkProperties.getControlParams());
+                        Log.i("ContentMetaData", "metadata " + lmUniversalObject.getMetadata());
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     };
@@ -174,4 +193,26 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void openActivity(String title, String param_view, String shareContent, String url_path, Class clazz) {
+        Intent intent = new Intent(MainActivity.this, clazz);
+        if (!TextUtils.isEmpty(title)) {
+            intent.putExtra(ShareActivity.TITLE, title);
+        }
+        if (!TextUtils.isEmpty(param_view)) {
+            intent.putExtra(ShareActivity.PARAM_VIEW, param_view);
+        }
+        if (!TextUtils.isEmpty(shareContent)) {
+            intent.putExtra(ShareActivity.SHARE_CONTENT, shareContent);
+        }
+        if (!TextUtils.isEmpty(url_path)) {
+            intent.putExtra(ShareActivity.URL_PATH, url_path);
+        }
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        simpleInitListener.reset();
+        setIntent(intent);
+    }
 }
---
+++ LinkedME-Demo/src/main/java/com/microquation/sample/activity/WBShareActivity.java
@@ -0,0 +1,10 @@
+package com.microquation.sample.activity;
+
+import com.umeng.socialize.media.WBShareCallBackActivity;
+
+/**
+ * 该activity微博分享必须创建
+ * Created by LinkedME06 on 16/7/12.
+ */
+public class WBShareActivity  extends WBShareCallBackActivity {
+}
